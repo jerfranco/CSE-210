b5c95d22d370c558488cfb8581e164b26e232770
Jeremiah Franco <fra19006@byui.edu>
2023-04-21 11:18:53 -0600

    Deleted Hello Prep 2 world and added my code

--- csharp-prep/Prep2/Program.cs
+++ csharp-prep/Prep2/Program.cs
@@ -4,6 +4,29 @@ class Program
 {
     static void Main(string[] args)
     {
-        Console.WriteLine("Hello Prep2 World!");
+        Console.Write("What is your grade percentage? ");
+        string input = Console.ReadLine();
+        int grade = int.Parse(input);
+        if (grade >= 90){
+            Console.WriteLine("A");
+        }
+        else if (grade <= 89 && grade >= 80){
+            Console.WriteLine("B");
+        }
+        else if (grade <=79 && grade >= 70){
+            Console.WriteLine("C");
+        }
+        else if (grade <= 69 && grade >= 60){
+            Console.WriteLine("D");
+        }
+        else{
+            Console.WriteLine("F");
+        }
+        if (grade >= 70){
+            Console.WriteLine("You passed the course!");
+        }
+        else{
+            Console.WriteLine("I believe in you!!!!");
+        }
     }
 }
