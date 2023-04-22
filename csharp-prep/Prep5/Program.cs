6b1b480a59de47f9a869959ff9379696ba24aee5
Jeremiah Franco <fra19006@byui.edu>
2023-04-22 12:42:16 -0600

    Changed code for Prep 5 for assignment

--- csharp-prep/Prep5/Program.cs
+++ csharp-prep/Prep5/Program.cs
@@ -4,6 +4,36 @@ class Program
 {
     static void Main(string[] args)
     {
-        Console.WriteLine("Hello Prep5 World!");
+        welcome();
+        string username = userNameInput();
+        int number = numberInput();
+        int sanswer = squared(number);
+        response(username, sanswer);
+
+    }
+    static void welcome() 
+    {
+        Console.WriteLine("Welcome to the program!");
+    }
+    static string userNameInput() 
+    {
+        Console.Write("Please enter your name: ");
+        string name = Console.ReadLine();
+        return name;
+    }
+    static int numberInput() 
+    {
+        Console.Write("Please enter your favorite number: ");
+        int favNumber = int.Parse(Console.ReadLine());
+        return favNumber;
+    }
+    static int squared(int favNumber) 
+    {
+        int answer = favNumber * favNumber;
+        return answer;
+    }
+    static void response(string name, int answer)
+    {
+        Console.WriteLine($"{name}, the square of your number is {answer}");
     }
 }
