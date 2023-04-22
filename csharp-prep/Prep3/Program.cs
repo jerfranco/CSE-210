e1ac99880552ad698984bd68c660fd3f21359cf2
Jeremiah Franco <fra19006@byui.edu>
2023-04-22 11:37:56 -0600

    Editted Prep3 code and added code for assignment

--- csharp-prep/Prep3/Program.cs
+++ csharp-prep/Prep3/Program.cs
@@ -4,6 +4,23 @@ class Program
 {
     static void Main(string[] args)
     {
-        Console.WriteLine("Hello Prep3 World!");
+
+        Random randomGenerator = new Random();
+        int magic = randomGenerator.Next(1, 100);
+        
+        int guess = 1;
+        while (guess != magic) {
+            Console.Write("What is your guess? ");
+            guess = int.Parse(Console.ReadLine());
+            if (guess > magic) {
+                Console.WriteLine("Lower");
+            }
+            else if (guess < magic) {
+                Console.WriteLine("Higher");
+            }
+            else {
+                Console.WriteLine("You guessed it!");
+            }
+        }
     }
 }
